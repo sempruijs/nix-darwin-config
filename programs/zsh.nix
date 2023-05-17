a867e607a307b848c382f29800c6f77e73e4bf93
sempruijs <sem@pruijs.net>
2023-05-17 11:52:47 +0200

    add cargo build allias

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -7,8 +7,12 @@
     lg = "lazygit";
     hxt = "CARGO_TARGET_DIR=target/rust-analyzer nix run github:pinelang/helix-tree-explorer/tree_explore"; # Helix PR with tree explorer
     nd = "nix develop --command zsh";
+
+    # quick cargo commands
     ck = "cargo check";
     cr = "cargo run";
+    cb = "cargo build";
+
     pr-status = "watch -n 10 gh pr status";
     hs = "nix-shell -p ghc";
     rinit = "nix flake init -t github:cor/flake-templates\#rust";
