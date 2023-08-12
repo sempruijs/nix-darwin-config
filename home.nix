143a968c690ab2a86ec2d101b62de8a8baa35d35
sempruijs <sem@pruijs.net>
2023-08-12 20:45:31 +0200

    add gomuks

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@
         bat
         unixtools.watch
         qrencode
+        gomuks
 
         inputs.nrc.packages.${pkgs.system}.default
         inputs.justhash.packages.${pkgs.system}.default
